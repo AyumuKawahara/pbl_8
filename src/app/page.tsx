b4e33c92210949131d21ec068df20af0fb1a1699
2a1o2u1e1i <ykiginir151216@docomo.ne.jp>
2024-06-14 18:00:43 +0900

    今日のPBLで作業した分を変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { univs } from '@/db/universities'
+
 export default function Home() {
-  return <div>hello</div>;
+  const data = univs
+  return (
+    <div>
+      <div>{data[0].id}</div> <div>{data[1].name}</div>
+    </div>
+  )
 }
---
+++ src/db/universities.ts
@@ -0,0 +1,8 @@
+export const univs = [
+  { id: 1, name: 'osaka' },
+  { id: 2, name: 'tokyo' },
+]
+
+export const usr = [{ bunkei: 50, rikei: 50 }]
+// bunkei:文系度，rikei:理系度
+// 足したら100になるように設定(そうしなくてもいい)
