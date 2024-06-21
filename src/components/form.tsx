45544d7c5748153c15c645707071b5ae55159344
wadon1011 <kousuke20031011@yahoo.co.jp>
2024-06-21 16:12:08 +0900

    フォームの型を作りました

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,3 +1,14 @@
 export const Form = () => {
-  return <form>フォーム</form>
+  return <form>
+    <form>
+      <p>質問</p>
+      <div>
+        <label htmlFor="q1-y">Yes</label>
+        <input type="radio" id="q1-y" value="yes" className="hidden"/>
+        <label htmlFor="q1-n">No</label>
+        <input type="radio" id="q1-n" value="no" className="hidden"/>
+      </div>
+      <button type="submit" className="bg-blue-600 font-semibold text-lg px-3 py-1 rounded-md text-white">診断</button>
+    </form>
+  </form>
 }
