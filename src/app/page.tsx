6c15b725fc56676b05316bc0fa43b217c86f27bd
wadon1011 <kousuke20031011@yahoo.co.jp>
2024-06-28 16:01:52 +0900

    タイトル追加しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Form } from '@/components/form'
+import { Header } from '@/components/header'
 import { Univs } from '@/components/univs'
 import { univs } from '@/db/universities'
 
@@ -6,6 +7,7 @@ export default function Home() {
   const data = univs
   return (
     <div className="flex flex-col items-center">
+      <Header />
       <Form />
       <Univs />
     </div>
