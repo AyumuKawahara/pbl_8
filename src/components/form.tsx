9655a259f9c67fbff3e764dfa60cc5c3dbe14197
wadon1011 <kousuke20031011@yahoo.co.jp>
2024-06-28 16:37:35 +0900

    データを送信できるようにしました

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,16 @@
+import { answerAtom } from '@/atom/atom'
+import { useAtom } from 'jotai'
+import { useForm } from 'react-hook-form'
+
 export const Form = () => {
+  const { register, handleSubmit } = useForm()
+  const [answer, setAnswer] = useAtom(answerAtom)
+  const onSubmit = (data) => {
+    console.log(data)
+    setAnswer(data)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <p className="flex items-center justify-between text-lg px-10 w-full">
         質問
       </p>
@@ -10,18 +20,30 @@ export const Form = () => {
       <div className="flex items-center justify-between px-10 w-full">
         <label
           htmlFor="q1-y"
-          className="bg-sky-400 font-semibold text-lg px-3 py-1 m-5 rounded-md text-white"
+          className="bg-sky-400 hover:cursor-pointer font-semibold text-lg px-3 py-1 m-5 rounded-md text-white"
         >
           Yes
         </label>
-        <input type="radio" id="q1-y" value="yes" className="hidden" />
+        <input
+          type="radio"
+          id="q1-y"
+          value="yes"
+          className="hidden"
+          {...register('q1')}
+        />
         <label
           htmlFor="q1-n"
-          className="bg-rose-600 font-semibold text-lg px-3 py-1 m-5 rounded-md text-white"
+          className="bg-rose-600 hover:cursor-pointer font-semibold text-lg px-3 py-1 m-5 rounded-md text-white"
         >
           No
         </label>
-        <input type="radio" id="q1-n" value="no" className="hidden " />
+        <input
+          type="radio"
+          id="q1-n"
+          value="no"
+          className="hidden "
+          {...register('q1')}
+        />
       </div>
       <button
         type="submit"
