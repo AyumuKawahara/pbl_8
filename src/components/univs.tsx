dac58d1958863884acdae70c96708f515cd0fc3a
2a1o2u1e1i <ykiginir151216@docomo.ne.jp>
2024-07-05 15:51:37 +0900

    15:51現在の変更

--- src/components/univs.tsx
+++ src/components/univs.tsx
@@ -1,8 +1,20 @@
 import { Univ, univs } from '@/db/universities'
 
-export const stfunivs = univs.filter((univer) => univer.rikei >= 100)
+// export const stfunivs = univs.filter((univer) => univer.rikei >= 100)
 
-export const Univs = ({ stfunivs }: { stfunivs: Univ[] }) => {
+export const Univs = ({ answer }) => {
+  const user = {
+    rikei: 50,
+    bunkei: 50,
+  }
+
+  if(answer.q1 == "yes"){
+    user.rikei += 50
+  }else{
+    user.bunkei += 50
+  }
+
+  const stfunivs = univs.filter((univ) => univ.rikei <= user.rikei || univ.bunkei <= user.bunkei)
   return (
     <>
       {stfunivs.map((univ) => (
