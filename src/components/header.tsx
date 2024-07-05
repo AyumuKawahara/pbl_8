5749cfd56f52ea78bad228a011c2491e38004116
wadon1011 <kousuke20031011@yahoo.co.jp>
2024-07-05 15:59:49 +0900

    質問の追加とワニ博士の透過処理をしました

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,16 +2,16 @@ import Image from 'next/image'
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between px-12 w-full bg-blue-400">
-      <div className="bg-sky-200 p-2 rounded-md px-6 py-4">
-        <p className="text-9xl font-bold px-12">進路相談室</p>
-        <p className="text-7xl font-bold">教えて！アリゲーター先生</p>
+    <header className="flex items-center justify-between px-12 w-full bg-teal-500">
+      <div className="bg-[#c3f2ff] p-2 rounded-md px-6 py-4">
+        <p className="text-8xl font-bold px-12">進路相談室</p>
+        <p className="text-5xl font-bold">教えて！アリゲーター先生</p>
       </div>
       <div>
         <Image
-          src="/wanigator.jpg"
+          src="/アリゲーター1.png"
           alt="アリゲーター先生"
-          width={500}
+          width={414}
           height={100}
         />
       </div>
